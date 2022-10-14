d9c24d58b142a09621719b27b779937f845711de
indra <indra@indras-MacBook-Air.local>
2022-10-14 09:15:20 +0700

    add organization structure, footer

--- components/Shared/CardCarousel.js
+++ components/Shared/CardCarousel.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import ReactCardCarousel from "react-card-carousel";
-
+import { Image, Text } from "@chakra-ui/react";
 export default function MyCarousel() {
   const CONTAINER_STYLE = () => {
     return {
@@ -19,7 +19,7 @@ export default function MyCarousel() {
     return {
       height: "400px",
       width: "300px",
-      paddingTop: "80px",
+      // paddingTop: "80px",
       textAlign: "center",
       background: "#52C0F5",
       color: "#FFF",
@@ -33,11 +33,107 @@ export default function MyCarousel() {
   return (
     <div style={CONTAINER_STYLE()}>
       <ReactCardCarousel autoplay={true} autoplay_speed={2500}>
-        <div style={CARD_STYLE()}>First Card</div>
-        <div style={CARD_STYLE()}>Second Card</div>
-        <div style={CARD_STYLE()}>Third Card</div>
-        <div style={CARD_STYLE()}>Fourth Card</div>
-        <div style={CARD_STYLE()}>Fifth Card</div>
+        <div style={CARD_STYLE()}>
+          <Image
+            // maxWidth={"100%"}
+            // maxHeight="100%"
+            objectFit={"cover"}
+            height="90%"
+            width={"100%"}
+            src="https://w0.peakpx.com/wallpaper/223/397/HD-wallpaper-captain-america-avengers-endgame-captain-america-marvel-marvel-comics-marvel-superheroes-superheroes.jpg"
+          />
+          <Text
+            textAlign={{ lg: "center" }}
+            fontSize={{ lg: "md", base: "md" }}
+            fontWeight="bold"
+            color="white"
+            marginTop="2"
+            zIndex={"10"}
+          >
+            Dekan - Pahlevi Aulia
+          </Text>
+        </div>
+        <div style={CARD_STYLE()}>
+          <Image
+            objectFit={"cover"}
+            // maxWidth={"100%"}
+            // maxHeight="100%"
+            height="90%"
+            width={"100%"}
+            src="https://w0.peakpx.com/wallpaper/602/994/HD-wallpaper-iron-man-marvel-comics.jpg"
+          />
+          <Text
+            textAlign={{ lg: "center" }}
+            fontSize={{ lg: "md", base: "md" }}
+            fontWeight="bold"
+            color="white"
+            marginTop="2"
+            zIndex={"10"}
+          >
+            Wakil Dekan - Enrinal Zulhimar
+          </Text>
+        </div>
+        <div style={CARD_STYLE()}>
+          <Image
+            objectFit={"cover"}
+            // maxWidth={"100%"}
+            // maxHeight="100%"
+            height="90%"
+            width={"100%"}
+            src="https://wallpaperaccess.com/full/893015.jpg"
+          />
+          <Text
+            textAlign={{ lg: "center" }}
+            fontSize={{ lg: "md", base: "md" }}
+            fontWeight="bold"
+            color="white"
+            marginTop="2"
+            zIndex={"10"}
+          >
+            Kepala Prodi - Anisa Tahira
+          </Text>
+        </div>
+        <div style={CARD_STYLE()}>
+          <Image
+            objectFit={"cover"}
+            // maxWidth={"100%"}
+            // maxHeight="100%"
+            height="90%"
+            width={"100%"}
+            src="https://w0.peakpx.com/wallpaper/245/353/HD-wallpaper-doctor-strange-marvel.jpg"
+          />
+          <Text
+            textAlign={{ lg: "center" }}
+            fontSize={{ lg: "md", base: "md" }}
+            fontWeight="bold"
+            color="white"
+            marginTop="2"
+            zIndex={"10"}
+          >
+            Kepala Lab - Tandry Syawaludin
+          </Text>
+        </div>
+        <div style={CARD_STYLE()}>
+          {" "}
+          <Image
+            objectFit={"cover"}
+            // maxWidth={"100%"}
+            // maxHeight="100%"
+            height="90%"
+            width={"100%"}
+            src="https://w0.peakpx.com/wallpaper/1013/119/HD-wallpaper-marvel-shang-chi.jpg"
+          />
+          <Text
+            textAlign={{ lg: "center" }}
+            fontSize={{ lg: "md", base: "md" }}
+            fontWeight="bold"
+            color="white"
+            marginTop="2"
+            zIndex={"10"}
+          >
+            Security - Indra Ahmad
+          </Text>
+        </div>
       </ReactCardCarousel>
     </div>
   );
